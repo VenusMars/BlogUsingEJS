8049e3002b1b111a4888a95d9ee9cddd345af1c3
Ramya <rramya.jupiter@gmail.com>
2023-02-26 15:29:45 +0530

    Initial commit with dummy data for the blog using mongodb

--- app.js
+++ app.js
@@ -4,38 +4,50 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Motivation is the driving force behind everything we do. It's what gets us out of bed in the morning and helps us tackle our daily tasks with enthusiasm. Without motivation, we can quickly lose sight of our goals and aspirations. But when we are motivated, we have the power to achieve great things. Whether it's starting a new project, pursuing a passion, or simply trying to get through a tough day, motivation is essential. By finding ways to stay motivated, we can overcome obstacles, stay focused on our objectives, and reach new heights of success.";
+const aboutContent = "Learn more about our mission, values, and commitment to providing exceptional customer experiences. Discover our history, achievements, and vision for the future. Meet our team of experts, who are dedicated to helping you achieve your goals and stay motivated every step of the way. Our About Us menu is the perfect place to learn more about who we are and what we stand for.";
+const contactContent = " Contact our customer service team for assistance with orders, products, or general inquiries. Reach out to our experts for guidance and advice on staying motivated and achieving your goals. You can also join our community by subscribing to our newsletter or following us on social media. We are here to support you in your journey towards success and we look forward to hearing from you.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb+srv://admin_ramya:testmongodb123@cluster0.blqgvvm.mongodb.net/blogDB");
+//mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+mongoose.set('strictQuery', true);
+
+// Create Schema
+const postSchema = {
+  title : String,
+  content : String,
+};
+const Post = mongoose.model("post",postSchema);
+
+let foundPosts=[];
 app.get("/",function(req,res){
-  res.render("home",{
-    homeContext:homeStartingContent,
-    postContent:posts
+  Post.find({},function(err,foundPosts){
+    if(!err){
+      res.render("home",{
+        homeContext:homeStartingContent,
+        postContent:foundPosts
+        });
+    }
   });
 });
 
 app.get("/posts/:postid",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.postid);
-
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle == requestedTitle){
-        res.render("post",{
-          postTitle : post.title,
-          postMessage : post.content
-        })
+  const requestedTitle = _.capitalize(req.params.postid); 
+  Post.findOne({title:requestedTitle},function(err,foundPosts){
+    if(!err){
+      res.render("post", {postTitle: foundPosts.title, postMessage: foundPosts.content});
+    }else{
+      console.log("Requested post not found");
     }
-    })
+  });
 });
 
 app.get("/about",function(req,res){
@@ -51,22 +63,17 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  const post = {
-    title : req.body.postTitle,
+  const post = new Post({
+    title : _.capitalize(req.body.postTitle),
     content : req.body.postBody,
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT ||3000, function() {
   console.log("Server started on port 3000");
 });
